06e61e4be67144ad1ffbdde9732c0de7a2e5f40c
강수진 <rkdthd1234@naver.com>
2019-07-25 02:17:59 +0900

    아이템 리스트 보기 api 작성

--- src/service/itemService.js
+++ src/service/itemService.js
@@ -1,9 +1,71 @@
 const itemDao = require('../dao/itemDao');
 
 async function getItem(categoryIdx, order) {
-    const result = await itemDao.selectItem(categoryIdx, order);
+    const result = {};
+    const items = [];
+    let itemIdxs;
+    //categoryIdx -1 이면 전체 item반환
+    if (categoryIdx == -1) {
+        itemIdxs = await itemDao.selectAllItemIdx();
+      } else {
+        itemIdxs = await itemDao.selectItemIdx(categoryIdx);
+    }
 
+    const itemsLength = itemIdxs.length;
+    for (let i = 0; i < itemsLength; i++) {
+      const itemIdx = itemIdxs[i].item_idx;
+      const item = await itemDao.selectItemDetail(itemIdx);
+      const storeIdx = item[0].store_idx;
+      const store = await itemDao.selectStoreDetail(storeIdx);
+      delete item[0].store_idx;
+      delete item[0].date;
+      item[0].store = store[0].name;
+      item[0].email = store[0].email;
+      item[0].facebook = store[0].facebook;
+      items.push(item[0]);
+    }
+    result.total_cnt = itemsLength;
+    //order -> 0 : report_cnt, 1 : date, 2 : name 
+    const orderType = getOrderType(order);
+    sort(items, orderType.name, orderType.desc);
+    console.log(items);
+    result.items = items;
     return result;
+}
+
+function getOrderType(order) {
+    let name;
+    let desc;
+    switch(order) {
+        case "0":
+            name = "report_cnt";
+            desc = true;
+            break;
+        case "1":
+            name = "date";
+            desc = true;
+          break;
+        case "2":
+            name = "name";
+            desc = false;
+          break;
+        default:
+            name = "report_cnt";
+            desc = true
+      }
+      return {name : name, desc :desc};
+}
+
+function sort(dataArr, attr, desc) {
+    dataArr.sort(function(a, b) {
+        var reportA = a[attr];
+        var reportB = b[attr];
+        if (desc) {
+            return (reportA > reportB) ? -1 : (reportA < reportB) ? 1 : 0;
+        } else {
+            return (reportA < reportB) ? -1 : (reportA > reportB) ? 1 : 0;
+        }
+    });
 }
 
 async function addReport(itemIdx) {
