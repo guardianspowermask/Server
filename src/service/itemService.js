619aa390c1929b71f0a6cf6dead1d23a9437fce0
sovovy <sovovy@gmail.com>
2019-08-14 20:47:54 +0900

    feedback img s3location 잇기

--- src/service/itemService.js
+++ src/service/itemService.js
@@ -15,16 +15,19 @@ async function getItem(categoryIdx, order) {
 
     const itemsLength = itemIdxs.length;
     for (let i = 0; i < itemsLength; i++) {
-      const itemIdx = itemIdxs[i].item_idx;
-      const item = await itemDao.selectItemDetail(itemIdx);
-      item[0].img = s3Location+item[0].img
-      const storeIdx = item[0].store_idx;
-      const store = await itemDao.selectStoreDetail(storeIdx);
-      delete item[0].store_idx;
-      item[0].store = store[0].name;
-    //   item[0].email = store[0].email;
-    //   item[0].facebook = store[0].facebook;
-      items.push(item[0]);
+        const itemIdx = itemIdxs[i].item_idx;
+        const item = await itemDao.selectItemDetail(itemIdx);
+        item[0].img = s3Location+item[0].img
+        if(item[0].feedback_img.length!=0){
+            item[0].feedback_img = s3Location + item[0].feedback_img;
+        }
+        const storeIdx = item[0].store_idx;
+        const store = await itemDao.selectStoreDetail(storeIdx);
+        delete item[0].store_idx;
+        item[0].store = store[0].name;
+        //   item[0].email = store[0].email;
+        //   item[0].facebook = store[0].facebook;
+        items.push(item[0]);
     }
     result.total_cnt = itemsLength;
     //sort -> 0 : report_cnt, 1 : date, 2 : name 
